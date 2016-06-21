4a9bd7a41180cb934420ebf3273fc46fc97dcc41
Derek Shiller <DCShiller@gmail.com>
2016-06-21 08:29:42 -0400

    add run_program method to program_reader

--- program_reader.rb
+++ program_reader.rb
@@ -1,26 +1,36 @@
-
-load 'machines_state.rb'
-
+require_relative 'machine_state'
+require_relative 'tape'
 
 class ProgramReader
 
-
 	def initialize
 		@counter = 0
+		@tape = Tape.new
+		null_state = MachineState.halt(0)
+		@program_start = null_state
+		@finished = false
 	end
 
 	def counter
 		@counter += 1
 	end
 
-
-	def define_program
-		adder = MachineState.new(counter)
-		adder.set_behavior({:1=>[:L,1] , :0=>[:x,0]})
+	def run_program
+		until @finished
+			@tape.render
+			current_symbol = @tape.get_symbol_under_reader
+			next_action = @program_state.get_behavior(current_symbol)
+			case next_action
+			when :halt
+				@finished = true
+				break
+			when :right
+			when :left
+			when :markx
+			when :mark0
+			end
+			@program_state = @program_state.get_next_state
+		end
 	end
 
-
-	def start 
-
-	end
 end
