f8c604f7e62e22f37ecfc892d3c3ce15ca27b656
dcshiller <dcshiller@gmail.com>
2016-06-21 08:30:14 -0400

    add halt state

--- machine_state.rb
+++ machine_state.rb
@@ -1,10 +1,16 @@
 
 class MachineState
-
 	attr_accessor :number, :instruction_hash
 
 	def initialize(state_number)
-		number = state_number		
+		@number = state_number
+		@instruction_hash = {}
+	end
+
+	def self.halt(state_number = 0)
+		halt_state = MachineState.new(state_number)
+		halt_state.number = state_number
+		halt_state.set_behavior({:x => :halt, :"0" => :halt})
 	end
 
 	def set_behavior(behavior_hash)
@@ -15,6 +21,5 @@ class MachineState
 		instruction_hash[input]
 	end
 
+
 end
-
-
