b4364e46e5e1a6b14fa686af713323cb0530858f
dcshiller <dcshiller@gmail.com>
2016-06-30 09:01:13 -0400

    add state names to program editor

--- lib/key_input.rb
+++ lib/key_input.rb
@@ -4,6 +4,8 @@ module KeyInput
 def get_keystroke
   STDIN.raw!
   key = STDIN.getc.chr
+  if key == "\e" then key << STDIN.read_nonblock(3) rescue nil end
+  if key == "\e" then key << STDIN.read_nonblock(2) rescue nil end
   STDIN.cooked!
   key
 end
