efa4b1df812fc13e959f1ac960399650b05404ff
Derek Shiller <DCShiller@gmail.com>
2016-08-16 10:29:40 -0400

    procify strings

--- lib/menus/main_menu.rb
+++ lib/menus/main_menu.rb
@@ -5,7 +5,8 @@ require_relative 'program_editor'
 class MainMenu < Menu
   include WinOrg
 
-  MAIN_MENU_OPTIONS = ["What is a Turing Machine?",
+  MAIN_MENU_OPTIONS = [
+    "What is a Turing Machine?",
     "Watch Sample",
     "Edit/Write Program",
     "Load Program",
@@ -13,25 +14,28 @@ class MainMenu < Menu
     "Exit"
   ]
 
-  MAIN_MENU_EFFECTS = [ nil,
-    Proc.new {ProgramReader.new.run_program},
-    " ProgramEditor.new",
-    Proc.new {
-      begin
-        response = full_screen_gets('File name:').chomp
-        unless response == 'back'
-          file_name = './programs/' + response + '.tm'
-          yaml_program_states = File.read(file_name)
-          @program_states = YAML.load(yaml_program_states)
-        end
-      rescue
-        flash("file not found \n" + center("('back'returns to menu)"))
-        retry
-      end
-    },
-    " nil",
-    " full_clear and break"
+  MAIN_MENU_EFFECTS = [
+    Proc.new { },
+    Proc.new { ProgramReader.new.run_program },
+    Proc.new { ProgramEditor.new },
+    Proc.new { load_program },
+    Proc.new { },
+    Proc.new { full_clear; exit}
   ]
+
+  def load_program
+    begin
+      response = full_screen_gets('File name:' ).chomp
+      unless response == 'back'
+        file_name = './programs/' + response + '.tm'
+        yaml_program_states = File.read(file_name)
+        @program_states = YAML.load(yaml_program_states)
+      end
+    rescue
+      flash("file not found \n" + center("('back'returns to menu)"))
+      retry
+    end
+  end
 
   def initialize
     super(MAIN_MENU_OPTIONS, MAIN_MENU_EFFECTS, "Turing Machine Simulator")
