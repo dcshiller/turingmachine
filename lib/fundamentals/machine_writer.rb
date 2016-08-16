67c18187ee3fb829a656b7f0184d46ffaea922fc
dcshiller <dcshiller@gmail.com>
2016-08-16 18:41:48 -0400

    adjust file directory saved to

--- lib/fundamentals/machine_writer.rb
+++ lib/fundamentals/machine_writer.rb
@@ -11,4 +11,22 @@ module MachineWriter
     unyamlized_prog = YAML::load(File.read(file))
   end
 
+  def load_program
+    begin
+      files = Dir["./programs/*.tm"]
+      files.collect! {|filename| filename.split("/").last.split(".").first}
+      files.unshift("")
+      files.unshift("Files:")
+      files << ""
+      response = full_screen_gets('File name:', files ).chomp
+      unless response == 'back'
+        file_name = './programs/' + response +'.tm'
+        yaml_program_states = File.read(file_name)
+        $program = YAML.load(yaml_program_states).first
+      end
+    rescue
+      flash("file not found \n" + center("('back'returns to menu)"))
+      retry
+    end
+  end
 end
