abe2dcbcbb97198ba4d6118f753795f40845c967
Derek Shiller <DCShiller@gmail.com>
2016-08-16 10:54:28 -0400

    remove comments

--- lib/menus/splash.rb
+++ lib/menus/splash.rb
@@ -1,21 +1,12 @@
 require_relative '../fundamentals/window_organization'
 require_relative '../fundamentals/key_input'
-
-#module Splash
-#   include WinOrg, KeyInput
-
   def splash
     refresh_window_information
-    # system("clear")
     full_clear #
     puts "\n" * ((@rows / 2) - 2)
     title = "Turing Machine Simulator".split("")
     title = title.collect.with_index {|mark, idx| Space.new(Tape.color(idx), mark) }
     puts " " * ((@cols - title.length*3) /2) + title.join("") + " " * ((@cols - title.length*3) /2)
-    puts
-    center( "Derek Shiller (2016)")
-    #puts "\n" * (@rows / 2)
+    puts center( "Derek Shiller (2016)")
     get_keystroke
   end
-
-#end
